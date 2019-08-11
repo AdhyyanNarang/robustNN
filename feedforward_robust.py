Training works but some bug in FGSM gradients I think

feedforward_robust.py
@@ -22,6 +22,8 @@ class RobustMLP(object):
         self.hidden_sizes = hidden_sizes
         self.num_classes = num_classes
         (self.x_train, self.y_train), (self.x_test, self.y_test) = dataset
+        self.y_train = self.y_train.reshape(len(self.y_train), 1)
+        self.y_test = self.y_test.reshape(len(self.y_test), 1)
         self.writer = writer
         self.scope = scope
         self.logger = logger
@@ -38,11 +40,13 @@ class RobustMLP(object):
         self.activations, self.predictions = model(self.x, self.hidden_sizes, self.num_classes, sigma)
         self.featurizations = self.activations[-1]
 
-        self.loss_vector = tf.nn.softmax_cross_entropy_with_logits(logits=self.predictions, labels=self.y)
+        logits=tf.squeeze(self.predictions)
+        labels=tf.squeeze(self.y)
+        self.loss_vector = tf.losses.hinge_loss(labels = labels,logits = logits)
         self.loss = tf.reduce_mean(self.loss_vector)
         tf.summary.scalar("loss", self.loss)
 
-        self.correct_prediction = tf.equal(tf.argmax(self.predictions, 1), tf.argmax(self.y, 1))
+        self.correct_prediction = tf.equal(tf.argmax(self.predictions, -1), tf.argmax(self.y, -1))
         self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, "float"))
         tf.summary.scalar("accuracy", self.accuracy)
 
@@ -132,6 +136,7 @@ class RobustMLP(object):
         return tf.squeeze(x_adv)
 
     def fgsm_np(self, sess, X, y, eps):
+        y = y.reshape(len(y), 1)
         x_adv = self.fgsm(X, eps)
         feed_dict = {
             self.x : X,
@@ -150,7 +155,9 @@ class RobustMLP(object):
 
         #New predictions and loss - call to model will reuse learned weights
         activations, predictions = model(x_tilde, self.hidden_sizes, self.num_classes, self.sigma)
-        loss_vector = tf.nn.softmax_cross_entropy_with_logits(logits=predictions, labels=y_ph)
+        #loss_vector = tf.nn.softmax_cross_entropy_with_logits(logits=predictions, labels=y_ph)
+        loss_vector = tf.losses.hinge_loss(labels = y_ph,logits = predictions)
+
         loss_tilde = tf.reduce_mean(loss_vector)
 
         #Optimization
@@ -172,19 +179,12 @@ class RobustMLP(object):
         x_tilde_np = sess.run(x_tilde, feed_dict = feed_dict)
         return x_tilde_np
 
-    def pgd_np(self, sess, x, y, eps, eta, num_iter):
-        x_tilde = self.pgd(sess, x, y, eps, eta, num_iter)
-        feed_dict = {self.x: x, self.y : y}
-        x_tilde_np = sess.run(x_tilde, feed_dict = feed_dict)
-        return x_tilde_np
-
     def sample_attack(self, eps, num_samples = 100):
         """
         Returns only x_adv in the interest of generalizable code
         """
         #Repeat x num_sample times
         n, d = self.x.shape
-        #ipdb.set_trace()
         x_ext = tf.keras.backend.repeat(self.x, num_samples)
         big_shape = tf.shape(x_ext)
         x_ext = tf.reshape(x_ext, [-1, d])
@@ -241,16 +241,17 @@ class RobustMLP(object):
         correct_indices = [i for i,v in enumerate(pred) if np.argmax(v) == np.argmax(y_test[i])]
 
         overall, overall_std  = self.dist_average(sess, x_test, x_test_adv, order = order)
-        correct_dist, correct_std = self.dist_average(sess, x_test[correct_indices], x_test_adv[correct_indices], order)
-        incorrect_dist, incorrect_std = self.dist_average(sess, x_test[incorrect_indices], x_test_adv[incorrect_indices], order)
+        #correct_dist, correct_std = self.dist_average(sess, x_test[correct_indices], x_test_adv[correct_indices], order)
+        #incorrect_dist, incorrect_std = self.dist_average(sess, x_test[incorrect_indices], x_test_adv[incorrect_indices], order)
 
-        return overall, overall_std, correct_dist, correct_std, incorrect_dist, incorrect_std
+        return overall, overall_std, None, None, None, None 
 
     def dist_average(self, sess, x_test, x_test_adv, order):
         distances = []
         for i in range(len(x_test)):
             dist = self.dist_calculator(sess, x_test[i], x_test_adv[i], order)
             distances.append(dist)
+        ipdb.set_trace()
         return np.average(distances, axis = 0), np.std(distances, axis = 0)
 
     def dist_calculator(self,sess, x, x_adv, order):
@@ -272,6 +273,7 @@ class RobustMLP(object):
             return distances
 
     def evaluate(self, sess, X, y):
+        y = y.reshape(len(y), 1)
         loss, accuracy = sess.run([self.loss, self.accuracy],
                                  feed_dict = {
                                      self.x : X,
@@ -280,6 +282,7 @@ class RobustMLP(object):
         return loss, accuracy
 
     def adv_evaluate(self, sess, X, y, eps, pgd = False, eta = 1e-2, num_iter = 500):
+        y = y.reshape(len(y), 1)
         if not pgd:
             X_adv = self.fgsm_np(sess, X, y, eps)
         else:
@@ -293,6 +296,7 @@ class RobustMLP(object):
         return loss, accuracy
 
     def fit_helper(self, sess, X, y, optimizer, loss, accuracy, lr = 0.003, training_epochs=15, batch_size=32, display_step=1):
+        y = y.reshape(len(y), 1)
         for epoch in range(training_epochs):
             avg_cost = 0.0
             total_batch = int(len(X) / batch_size)
@@ -342,10 +346,13 @@ class RobustMLP(object):
         return True
 
     def adv_fit(self, sess, X, y, eps, lr = 3e-4, training_epochs=15, batch_size=32, display_step=1):
+        y = y.reshape(len(y), 1)
         x_adv = self.fgsm(self.x, eps)
         _, predictions = model(x_adv, self.hidden_sizes, self.num_classes, self.sigma)
 
-        loss_vector = tf.nn.softmax_cross_entropy_with_logits(logits=predictions, labels=self.y)
+        #loss_vector = tf.nn.softmax_cross_entropy_with_logits(logits=predictions, labels=self.y)
+        loss_vector = tf.losses.hinge_loss(labels = self.y, logits = predictions)
+
         loss_adv = tf.reduce_mean(loss_vector)
         tf.summary.scalar("loss", loss_adv)
 

scripts/regular_training.py
@@ -24,9 +24,10 @@ eps_train = 0.1
 eps_test = 0.1
 tensorboard_dir = "tb/"
 weights_dir = "weights/"
-load_weights = False 
-load_counter = 58 
+load_weights = False
+load_counter = 58
 sigma = tf.nn.relu
+epochs, reg, lr = 20, 0.000, 3e-3
 
 #Configuring the logger
 
@@ -56,18 +57,39 @@ if __name__ == "__main__":
     #Setup - Dataset stuff
     mnist = tf.keras.datasets.mnist
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    y_test_ogi = y_test
-    x_test_ogi = x_test
+    y_train, y_test = y_train.astype('int'), y_test.astype('int')
+    indices_train = []
+    for idx, label in enumerate(y_train):
+        if label == 1 or label == 7:
+            indices_train.append(idx)
+    ones_test = np.argwhere(y_test== 1)[:, 0]
+    sevens_test = np.argwhere(y_test == 7)[:, 0]
+    indices_test= np.concatenate((ones_test, sevens_test))
+    x_train, y_train, x_test, y_test = x_train[indices_train], y_train[indices_train], x_test[indices_test], y_test[indices_test]
+
+    #Replace with labels as 1's and -1's
+    for idx, label in enumerate(y_train):
+        if y_train[idx] == 1:
+            y_train[idx] = -1
+        else:
+            y_train[idx] = 1
+
+    for idx, label in enumerate(y_test):
+        if y_test[idx] == 1:
+            y_test[idx] = -1
+        else:
+            y_test[idx] = 1
+
+
+
     x_train = x_train/255
     x_test = x_test/255
-    num_classes = 10
-    y_train = tf.keras.utils.to_categorical(y_train, num_classes)
-    y_test = tf.keras.utils.to_categorical(y_test, num_classes)
+    num_classes = 1
     x_train_flat, input_shape = flatten_mnist(x_train)
     x_test_flat, _ = flatten_mnist(x_test)
 
     sess = tf.Session()
-    hidden_sizes = [32,32,32,32,32,32,32]
+    hidden_sizes = [32,32,32]
     dataset = ((x_train_flat, y_train), (x_test_flat, y_test))
 
     scope_name = "model_non_robust"
@@ -82,7 +104,6 @@ if __name__ == "__main__":
             logger.info("Created model successfully. Now going to train")
 
             #TODO: Fit the model until convergence before running the distance experiments again
-            epochs, reg, lr = 70, 0.008, 3e-3
             model.fit(sess, x_train_flat,y_train, training_epochs = epochs, reg = reg , lr = lr)
 
             #Save weights
@@ -109,6 +130,7 @@ if __name__ == "__main__":
             norms_np = sess.run(norms)
             logger.info(norms_np)
 
+            ipdb.set_trace()
             overall, overall_std, correct, _, incorrect, _ = model.get_distance(sess, eps_test, x_test_flat, y_test)
             logger.info("---Distances----")
             logger.info(overall)
@@ -121,11 +143,10 @@ if __name__ == "__main__":
             #TSNE visualization of final layer.
             x_test_flat_adv = model.fgsm_np(sess, x_test_flat, y_test, eps_test)
             metadata_path = os.path.join(logdir, 'metadata.tsv')
-            write_metadata(metadata_path, y_test_ogi[0:1000])
+            write_metadata(metadata_path, y_test)
             sprite_path = os.path.join(logdir, 'sprite_images.png')
-            write_sprite_image(sprite_path, x_test_ogi[0:1000])
-            model.visualize_activation_tsne(sess, x_test_flat_adv[0:1000], 'metadata.tsv', 'sprite_images.png', logdir)
-
+            write_sprite_image(sprite_path, x_test)
+            model.visualize_activation_tsne(sess, x_test_flat_adv, 'metadata.tsv', 'sprite_images.png', logdir)
 
     else:
         with tf.variable_scope(scope_name) as scope:
@@ -184,7 +205,7 @@ if __name__ == "__main__":
                 #TSNE visualization of final layer.
                 x_test_flat_adv = model.fgsm_np(sess, x_test_flat, y_test, eps_test)
                 metadata_path = os.path.join(logdir, 'metadata.tsv')
-                write_metadata(metadata_path, y_test_ogi[0:1000])
+                write_metadata(metadata_path, y_test)
                 sprite_path = os.path.join(logdir, 'sprite_images.png')
-                write_sprite_image(sprite_path, x_test_ogi[0:1000])
+                write_sprite_image(sprite_path, x_test)
                 model.visualize_activation_tsne(sess, x_test_flat_adv[0:1000], 'metadata.tsv', 'sprite_images.png', logdir)